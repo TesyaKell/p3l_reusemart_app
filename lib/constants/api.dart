1595715f9073c54d11ae8dc820515fac14b905aa
TesyaKell <220711896@students.uajy.ac.id>
2025-05-31 18:32:36 +0700

    api

--- lib/constants/api.dart
+++ lib/constants/api.dart
@@ -1,7 +1,9 @@
 class Api {
   // Base URL
-  static const String baseUrl = 'http://192.168.18.104:8000/api';
-  // static const String baseUrl = 'http://10.0.2.2:8000/api'; // emulator
+  static const String baseUrl =
+      //ini jangan dihapus, dikomen aja kalau ga dipake
+      'https://cb9d-2a09-bac1-34e0-18-00-3c3-45.ngrok-free.app/api';
+  //static const String baseUrl = 'http://10.0.2.2:8000/api'; // emulator
 
   // Auth endpoints
   static const String login = '$baseUrl/login-mobile';
